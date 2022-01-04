6cfd113b2d17ef3708734cc9bbd18e0914930cdd
danielsollus <danielsuporteti@outlook.com.br>
2022-01-04 19:49:20 -0300

    Melhorando a implementação do metodo imprimirTabuleiro

--- Jogo-Xadrez-NetCore5/Tela.cs
+++ Jogo-Xadrez-NetCore5/Tela.cs
@@ -9,15 +9,30 @@ namespace Jogo_Xadrez_NetCore5
         {
             for (int i = 0; i < tabuleiro.linhas; i++)
             {
+                Console.Write(8 - i + "|");
                 for (int j = 0; j < tabuleiro.colunas; j++)
                 {
                     if (tabuleiro.peca(i, j) == null)
                         Console.Write("- ");
                     else
-                        Console.Write($"{tabuleiro.peca(i, j)} ");
+                    {
+                        imprimirPeca(tabuleiro.peca(i, j));
+                        Console.Write(" ");
+                    }
                 }
                 Console.WriteLine();
             }
+            Console.WriteLine("  A B C D E F G H");
+            Console.WriteLine("__________________");
+        }
+        public static void imprimirPeca(Peca peca)
+        {
+            
+                ConsoleColor corConsole = Console.ForegroundColor;
+                Console.ForegroundColor =(peca.cor == Cor.Preta)? ConsoleColor.Yellow: ConsoleColor.White;
+                Console.Write(peca);
+                Console.ForegroundColor = corConsole;
+
         }
     }
 }
