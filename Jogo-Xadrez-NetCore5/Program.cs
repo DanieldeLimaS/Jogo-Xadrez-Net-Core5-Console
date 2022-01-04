b044834a029b626f54cb4061246b03a37f969022
danielsollus <danielsuporteti@outlook.com.br>
2022-01-04 19:23:42 -0300

    Criando a classe PosicaoXadrez

--- Jogo-Xadrez-NetCore5/Program.cs
+++ Jogo-Xadrez-NetCore5/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Xadrez.Exceptions;
-using Xadrez.Pecas;
-using Xadrez.Tabuleiro;
+using Xadrez.Logica;
 
 namespace Jogo_Xadrez_NetCore5
 {
@@ -11,29 +10,33 @@ namespace Jogo_Xadrez_NetCore5
         {
             try
             {
-                Tabuleiro tabuleiro = new Tabuleiro(8, 8);
+                #region codigo comentado
+                //Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 7));
-                tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 7));
-                tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.Preta), new Posicao(0, 1));
-                tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.Preta), new Posicao(0, 6));
-                tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.Preta), new Posicao(0, 2));
-                tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.Preta), new Posicao(0, 5));
-                tabuleiro.colocarPeca(new Dama(tabuleiro, Cor.Preta), new Posicao(0, 3));
-                tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 4));
-                for (int i = 0; i < 8; i++)
-                {
-                    tabuleiro.colocarPeca(new Peao(tabuleiro, Cor.Preta), new Posicao(1, i));
-                }
-                Tela.imprimirTabuleiro(tabuleiro);
-
+                //tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
+                //tabuleiro.colocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 7));
+                //tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.Preta), new Posicao(0, 1));
+                //tabuleiro.colocarPeca(new Cavalo(tabuleiro, Cor.Preta), new Posicao(0, 6));
+                //tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.Preta), new Posicao(0, 2));
+                //tabuleiro.colocarPeca(new Bispo(tabuleiro, Cor.Preta), new Posicao(0, 5));
+                //tabuleiro.colocarPeca(new Dama(tabuleiro, Cor.Preta), new Posicao(0, 3));
+                //tabuleiro.colocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 4));
+                //for (int i = 0; i < 8; i++)
+                //{
+                //    tabuleiro.colocarPeca(new Peao(tabuleiro, Cor.Preta), new Posicao(1, i));
+                //}
+                //Tela.imprimirTabuleiro(tabuleiro);
+                #endregion
+                PosicaoXadrez posicao = new PosicaoXadrez('A', 7);
+                Console.WriteLine(posicao);
+                Console.WriteLine(posicao.toPosicao());
+                Console.ReadLine();
             }
             catch (TabuleiroException ex)
             {
                 Console.WriteLine(ex.Message);
             }
-            Console.ReadLine();
+
         }
     }
 }
